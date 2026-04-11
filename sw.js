6ce9d7a733efff5ccbe16fe57cbe8b4d315f503b
hazelchoi400 <hazel.choi@gmail.com>
2026-04-11 18:52:19 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,18 @@
-const CACHE = 'nutricale-v1';
+const CACHE = 'nutricale-v3';
 const ASSETS = [
   '/nutricale/',
   '/nutricale/index.html',
-  '/nutricale/manifest.json'
+  '/nutricale/manifest.json',
+  '/nutricale/icon.png'
 ];
-
-// Install: cache all assets
+ 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
-
-// Activate: clear old caches
+ 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -22,10 +21,10 @@ self.addEventListener('activate', e => {
   );
   self.clients.claim();
 });
-
-// Fetch: serve from cache, fall back to network
+ 
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
+ 
